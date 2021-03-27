3fd3f4b9c52bc814036fa7e766c7f4742ac658f3
Liviu Ionescu <ilg@livius.net>
2021-03-27 11:51:39 +0200

    tests/500-xpm: update title

--- test/tap/500-xpm.js
+++ test/tap/500-xpm.js
@@ -89,7 +89,8 @@ test('xpm -h (spawn)', async (t) => {
     t.equal(code, 0, 'exit 0')
     t.match(stdout, 'Usage: xpm <command>', 'has Usage')
 
-    t.match(stdout, 'The xPack package manager command line tool', 'has title')
+    t.match(stdout, 'The xPack dependencies manager command line tool',
+      'has title')
     t.match(stdout, 'xpm -h|--help', 'has -h|--help')
     t.match(stdout, 'xpm <command> -h|--help', 'has <command> -h|--help')
     t.match(stdout, 'xpm --version', 'has --version')
