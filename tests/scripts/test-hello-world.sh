878cf27357b9015773d74400482706642854e992
Liviu Ionescu <ilg@livius.net>
2022-08-14 12:09:21 +0300

    update/shorten copyright notices

--- tests/scripts/test-hello-world.sh
+++ tests/scripts/test-hello-world.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 # -----------------------------------------------------------------------------
-# This file is part of the xPack distribution.
-#   (https://xpack.github.io)
-# Copyright (c) 2020 Liviu Ionescu.
+# This file is part of the xPack project (https://xpack.github.io).
+# Copyright (c) 2020 Liviu Ionescu. All rights reserved.
 #
 # Permission to use, copy, modify, and/or distribute this software
 # for any purpose is hereby granted, under the terms of the MIT license.
+#
+# If a copy of the license was not distributed with this file, it can
+# be obtained from https://opensource.org/licenses/MIT/.
 # -----------------------------------------------------------------------------
 
 # -----------------------------------------------------------------------------
