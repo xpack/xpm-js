ae528fd69b634bfa8193c4b8a50467ae5b61536e
Liviu Ionescu <ilg@livius.net>
2024-12-20 17:34:20 +0200

    website: re-generate commons

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -79,8 +79,11 @@ const config: Config = {
   organizationName: 'xpack', // Usually your GitHub org/user name.
   projectName: 'xpm-js', // Usually your repo name.
 
+  onBrokenAnchors: 'throw',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'throw',
+
+  onDuplicateRoutes: 'throw',
 
   // Useful for the sitemap.xml, to avoid redirects, since
   // GitHub redirects all to trailing slash.
